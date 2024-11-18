9730b623ec2e719466271a4a19b10ed771c52c4c
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-18 15:32:21 -0800

    navigate play button on scoreboard to quizpage

--- sba-react-web-app/src/components/Scoreboard.jsx
+++ sba-react-web-app/src/components/Scoreboard.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Scoreboard({ score, onPlayAgain }) {
+  const navigate = useNavigate(); // Initialize navigate function
+  const onPlayAgain1 = () => {
+    navigate("/quiz"); // Navigate to the quiz page
+  };
   return (
     <div className="scoreboard-container" style={{ marginTop: "2rem" }}>
       <h2 className="scoreboard-title">Game Over!</h2>
       {/*<h3 className="scoreboard-score">Your Score: {score}</h3>*/}
-      <button className="scoreboard-button" onClick={onPlayAgain}>
+      <button className="scoreboard-button" onClick={onPlayAgain1}>
         Play Again
       </button>
       <p className="scoreboard-note" style={{ fontStyle: "italic" }}>
